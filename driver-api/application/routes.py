d4e03169de5dac9952bb41fce55c7e23329a3856
jamalh8 <jamalh.888@gmail.com>
2022-06-28 19:41:18 +0000

    added routes file for driver-api

---
+++ driver-api/application/routes.py
@@ -0,0 +1,12 @@
+from application import app
+import random
+
+@app.route('/get_driver', methods=['GET'])
+def get_driver():
+    drivers = ['Valtteri Bottas', 'Guanyu Zhou', 'Pierre Gasly', 'Yuki Tsunoda', 'Fernando Alonso', 
+    'Esteban Ocon', 'Sebastian Vettel', 'Lance Stroll', 'Charles Leclerc', 'Carlos Sainz Jr.', 
+    'Mick Schumacher', 'Kevin Magnussen', 'Daniel Ricciardo', 'Lando Norris', 'Lewis Hamilton', 
+    'George Russell', 'Max Verstappen', 'Sergio Perez', 'Alex Albon', 'Nicholas Latifi']
+    driver = random.choice(drivers)
+    return driver
+
