6173be56ab058e25d0cbe6cbd9498a71d7250d6e
jamalh8 <jamalh.888@gmail.com>
2022-06-28 20:14:41 +0000

    test script written to automate testing

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+declare -a directories=("front-end" "driver-api" "car-api" "rating-api")
+for dir in "${directories[@]}"
+do
+  cd ${dir}
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip python3-venv -y
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r test_requirements.txt
+  python3 -m pytest --cov=application
+  deactivate
+  cd ..
+done
