c0998b411b7e9c692fa511c2b4b05acd2f3cabe4
jamalh8 <jamalh.888@gmail.com>
2022-07-11 10:47:03 +0000

    rolling update test

--- driver-api/application/routes.py
+++ driver-api/application/routes.py
@@ -6,7 +6,7 @@ def get_driver():
     drivers = ['Valtteri Bottas', 'Guanyu Zhou', 'Pierre Gasly', 'Yuki Tsunoda', 'Fernando Alonso', 
     'Esteban Ocon', 'Sebastian Vettel', 'Lance Stroll', 'Charles Leclerc', 'Carlos Sainz Jr.', 
     'Mick Schumacher', 'Kevin Magnussen', 'Daniel Ricciardo', 'Lando Norris', 'Lewis Hamilton', 
-    'George Russell', 'Max Verstappen', 'Sergio Perez', 'Alex Albon', 'Nicholas Latifi','Ayrton Senna', 'Niki Lauda', 'Nigel Mansell', 'Michael Schumacher', 'Martin Brundle']
+    'George Russell', 'Max Verstappen', 'Sergio Perez', 'Alex Albon', 'Nicholas Latifi']
     driver = random.choice(drivers)
     return driver
 
